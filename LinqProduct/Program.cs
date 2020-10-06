63efb4abbff9b55cb4a744067a8daae3e66a7ce4
wander0220 <s2018s26@e-mirim.hs.kr>
2020-10-06 09:52:47 +0900

    초기 커밋.

---
+++ LinqProduct/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqProduct
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+
+        public override string ToString()
+        {
+            return Name + " : " + Price + "원";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> input = new List<Product>()
+            {
+                new Product(){Name="고구마",Price=1500}
+                ,new Product(){Name="사과",Price=2400}
+                ,new Product(){Name="바나나",Price=1000}
+                ,new Product(){Name="배",Price=3000}
+                ,new Product(){Name="감자",Price=1000}
+                ,new Product(){Name="토마토",Price=2000}
+                ,new Product(){Name="옥수수",Price=1500}
+                ,new Product(){Name="자두",Price=500}
+            };
+        }
+    }
+}
